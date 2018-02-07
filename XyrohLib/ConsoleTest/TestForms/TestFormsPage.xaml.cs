1bdd40837c562361bb2fc393bea90259a3e40575
Andy Flisher <af@xyroh.com>
2018-02-07 22:20:58 +0000

    test xamarin forms project

---
+++ XyrohLib/ConsoleTest/TestForms/TestFormsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TestForms
+{
+    public partial class TestFormsPage : ContentPage
+    {
+        public TestFormsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
